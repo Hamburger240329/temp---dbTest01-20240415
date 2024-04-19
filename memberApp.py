094938ce10443e040bcd86aad4b7d1261ab08a9e
gyojincompany <luckyred@gmail.com>
2024-04-19 14:52:25 +0900

    회원관리앱 회원 조회 기능 추가

--- memberApp.py
+++ memberApp.py
@@ -15,6 +15,8 @@ class MainWindow(QMainWindow, form_class):
         self.join_btn.clicked.connect(self.member_join)  # 회원가입이 버튼이 클릭되면 가입함수 호출
         self.joinreset_btn.clicked.connect(self.join_reset)  # 초기화 버튼이 클릭되면 입력내용 초기화
         self.idcheck_btn.clicked.connect(self.idcheck)  # 회원가입여부 체크 버튼이 클릭되면 가입여부확인 함수 호출
+        self.membersearch_btn.clicked.connect(self.member_search)  # 회원조회 버튼 클릭시 회원정보 출력
+        self.memberreset_btn.clicked.connect(self.memberInfo_reset)  # 초기화 버튼 클릭되면 입력내용 초기화
 
 
     def member_join(self):  # 회원 가입 이벤트 처리 함수
@@ -85,6 +87,62 @@ class MainWindow(QMainWindow, form_class):
 
             cur.close()
             dbConn.close()
+
+    def member_check(self):  # 회원가입여부 체크 함수
+        memberid = self.memberid_edit.text()
+
+        dbConn = pymysql.connect(user="root", password="12345", host="localhost", db="shopdb")
+
+        sql = f"SELECT count(*) FROM appmember WHERE memberid='{memberid}'"
+        # SQL문 실행 시 1 또는 0이 반환(기존에 가입된 아이디면 1, 아니면 0)
+
+        cur = dbConn.cursor()
+        cur.execute(sql)  # 회원가입하는 sql문이 성공하면 1이 반환
+
+        result = cur.fetchall()
+
+        print(result)
+
+        if result[0][0] == 1:
+            return 1
+        else:
+            QMessageBox.warning(self, "회원조회불가", "가입이 되지 않은 아이디입니다.\n다른 아이디를 입력해주세요.")
+            return 0
+
+            cur.close()
+            dbConn.close()
+
+    def member_search(self):  # 아이디로 회원을 조회하는 함수
+
+        memberid = self.memberid_edit.text()  # 유저가 입력한 아이디 텍스트 가져오기
+        dbConn = pymysql.connect(user="root", password="12345", host="localhost", db="shopdb")
+
+        sql = f"SELECT * FROM appmember WHERE memberid='{memberid}'"
+        # SQL문 실행 시 1 또는 0이 반환(기존에 가입된 아이디면 1, 아니면 0)
+
+        if memberid == "":
+            QMessageBox.warning(self, "아이디입력오류", "아이디는 필수 입력사항입니다.\n아이디를 입력해주세요.")
+        elif self.member_check() == 0:
+            pass
+        else:
+            cur = dbConn.cursor()
+            cur.execute(sql)  # 회원가입하는 sql문이 성공하면 1이 반환
+
+            result = cur.fetchall()
+
+            print(result)
+
+            self.memberpw_edit.setText(str(result[0][1]))  # 비밀번호 출력
+            self.membername_edit.setText(str(result[0][2]))  # 회원이름 출력
+            self.memberemail_edit.setText(str(result[0][3]))  # 이메일 출력
+            self.memberage_edit.setText(str(result[0][4]))  # 회원나이 출력
+
+    def memberInfo_reset(self):  # 회원조회정보 입력내용 초기화
+        self.memberid_edit.clear()
+        self.memberpw_edit.clear()
+        self.membername_edit.clear()
+        self.memberemail_edit.clear()
+        self.memberage_edit.clear()
 
 
 
